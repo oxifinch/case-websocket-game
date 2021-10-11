cd6a8a52c6f756e378d3a33f60c71dfd4931964a
Eleonorepetersson <20eleonore.petersson@glimnet.se>
2021-10-11 10:58:12 +0200

    small styling and canvas

--- public/game.js
+++ public/game.js
@@ -1,1 +1,6 @@
 const websocket = new WebSocket("ws://localhost:8081");
+
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 640;
+canvas.height = 480;
