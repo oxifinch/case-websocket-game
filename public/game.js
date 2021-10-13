fd0b65dcd7558dae91f439008dfc26e6c9753992
oxifinch <oxifinch@protonmail.com>
2021-10-13 12:08:30 +0200

    Added GameManager class

--- public/game.js
+++ public/game.js
@@ -5,11 +5,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 640;
 canvas.height = 480;
 
-var playerList = [new PlayerCharacter(50, 50, 100, 100, "./witch_placeholder.png", 0.2, "Joseph", null)];
-var mushroomList = [new Mushroom(100, 100, 50, 50, "./mushroom_placeholder.png", 500)];
+var gameManager = new GameManager(canvas, 3, 10);
 
 // CORE GAME FUNCTIONS
 function initGame() {
+
+    gameManager.init();
+    document.addEventListener("keydown", (e) => {
+        gameManager.playerList[0].move(e.key, deltaTime);
+    });
 
     // Starting the game loop
     gameLoop();
@@ -17,62 +21,51 @@ function initGame() {
 
 function checkCollisions() {
     // Loop through players and check if they collide with mushrooms
-    for(let i = 0; i < playerList.length; i++) {
+    for(let i = 0; i < gameManager.playerList.length; i++) {
 
-        const currentPlayer = playerList[i];
+        const currentPlayer = gameManager.playerList[i];
 
-        for(let j = 0; j < mushroomList.length; j++) {
+        for(let j = 0; j < gameManager.mushroomList.length; j++) {
 
-            const currentMushroom = mushroomList[i];
+            const currentMushroom = gameManager.mushroomList[i];
 
             if (
-                currentPlayer.xPos <= (currentMushroom.xPos + 32) &&
-                currentMushroom.xPos <= (currentPlayer.xPos + 32) &&
-                currentPlayer.yPos <= (currentMushroom.yPos + 32) &&
-                currentMushroom.yPos <= (currentPlayer.yPos + 32)
+                currentPlayer.xPos <= (currentMushroom.xPos + currentMushroom.width) &&
+                currentMushroom.xPos <= (currentPlayer.xPos + currentPlayer.width) &&
+                currentPlayer.yPos <= (currentMushroom.yPos + currentMushroom.height) &&
+                currentMushroom.yPos <= (currentPlayer.yPos + currentPlayer.height)
             ) {
                 if(currentMushroom.active) {
                     currentMushroom.pickUp(currentPlayer, ctx);
                 }
             }
         }
-        
     }
 }
 
-function resetGame() {
-    // Reset PLAYER positions
-    for(let i = 0; i < playerList.length; i++) {
-        playerList[i].xPos = canvas.width / 2;
-        playerList[i].yPos = canvas.height / 2;
-
-    }
-    
-    // Place random MUSHROOMS
-    for(let i = 0; i < mushroomList; i++) {
-        mushroomList[i].xPos = 32 + (Math.random() * (canvas.width - 64));
-        mushroomList[i].yPos = 32 + (Math.random() * (canvas.height - 64));
-
-    }
-};
-
 var deltaTime = 0;
 var lastFrame = 0;
-document.addEventListener("keydown", (e) => {
-    playerList[0].move(e.key, deltaTime);
-});
+
+function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for(let i = 0; i < gameManager.mushroomList.length; i++) {
+        const mushroom = gameManager.mushroomList[i];
+        mushroom.draw(ctx);
+    }
+
+    for(let i = 0; i < gameManager.playerList.length; i++) {
+        const playerCharacter = gameManager.playerList[i];
+        playerCharacter.draw(ctx);
+    }
+}
 
 function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
     // Getting/setting deltaTime
     deltaTime = arguments[0] - lastFrame;
     lastFrame = arguments[0]
 
-    // TODO: All moving sprites should be grouped in an array and have their
-    // draw methods called together
-    playerList[0].draw(ctx);
-    mushroomList[0].draw(ctx);
+    render();
     checkCollisions();
 
     requestAnimationFrame(gameLoop);
